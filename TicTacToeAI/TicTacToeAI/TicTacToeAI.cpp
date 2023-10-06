1e1f0a44877c468fcd3a4ffa8718eb45140dfb1d
David McDade <dmcdadedf@gmail.com>
2023-10-06 11:49:51 -0400

    Full checking for x and o

--- TicTacToeAI/TicTacToeAI/TicTacToeAI.cpp
+++ TicTacToeAI/TicTacToeAI/TicTacToeAI.cpp
@@ -1,43 +1,83 @@
 #include <iostream>
 
-const int size = 4;
+const int size = 3;
 
 bool checkIfXWins(char board[size][size], int moveX, int moveY, int size);
+bool checkIfOWins(char board[size][size], int moveX, int moveY, int size);
 void printBoard(char board[size][size]);
 
 int main()
 {
 	bool xWins = false;
+	bool oWins = false;
 
-	char mainBoard[size][size] = { { 'x','o','o','x'},
-								   { 'o','x','x','o'},
-								   { 'o','x','x','o'},
-								   { 'x','o','o','x'} };
+	char mainBoard[size][size] = { { 'x','_','_'},
+								   { 'x','_','_'},
+								   { 'x','_','_'} };
 
 	printBoard(mainBoard);
 
 	xWins = checkIfXWins(mainBoard, 0, 0, size);
+	oWins = checkIfOWins(mainBoard, 2, 2, size);
 	
-	std::cout << xWins;
+	if (xWins) {
+		std::cout << "X Wins";
+	}
+	if (oWins) {
+		std::cout << "O Wins";
+	}
+	
 }
 
 bool checkIfXWins(char board[][size], int moveX, int moveY, int size)
 {
 	int numInARow = 0;
-
-	std::cout << board[moveX][moveY] << "\n";
 	
 	//Check Diagonal 1
-	//if (moveX == moveY) 
-	//{
+	if (moveX == moveY) 
+	{
+		numInARow = 0;
 
-	//}
+		for (int i = 0; i < size; i++)
+		{
+			if (board[i][i] == 'X' || board[i][i] == 'x')
+			{
+				numInARow++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		if (numInARow == size)
+		{
+			return true;
+		}
+	}
 
 	////Check Diagonal 2
-	//if (moveX + moveY == size - 1) 
-	//{
+	if (moveX + moveY == size - 1) 
+	{
+		numInARow = 0;
 
-	//}
+		for (int i = 0; i < size; i++)
+		{
+			if (board[size - i - 1][i] == 'X' || board[size - i - 1][i] == 'x')
+			{
+				numInARow++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		if (numInARow == size)
+		{
+			return true;
+		}
+	}
 
 	//Check Horozontal
 
@@ -84,13 +124,108 @@ bool checkIfXWins(char board[][size], int moveX, int moveY, int size)
 	return false;
 }
 
+bool checkIfOWins(char board[][size], int moveX, int moveY, int size)
+{
+	int numInARow = 0;
+
+	//Check Diagonal 1
+	if (moveX == moveY)
+	{
+		numInARow = 0;
+
+		for (int i = 0; i < size; i++)
+		{
+			if (board[i][i] == 'O' || board[i][i] == 'o')
+			{
+				numInARow++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		if (numInARow == size)
+		{
+			return true;
+		}
+	}
+
+	////Check Diagonal 2
+	if (moveX + moveY == size - 1)
+	{
+		numInARow = 0;
+
+		for (int i = 0; i < size; i++)
+		{
+			if (board[size - i - 1][i] == 'O' || board[size - i - 1][i] == 'o')
+			{
+				numInARow++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		if (numInARow == size)
+		{
+			return true;
+		}
+	}
+
+	//Check Horozontal
+
+	numInARow = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (board[i][moveY] == 'O' || board[i][moveY] == 'o')
+		{
+			numInARow++;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	if (numInARow == size)
+	{
+		return true;
+	}
+
+	//Check Vertical
+
+	numInARow = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (board[moveX][i] == 'O' || board[moveX][i] == 'o')
+		{
+			numInARow++;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	if (numInARow == size)
+	{
+		return true;
+	}
+
+	return false;
+}
+
 void printBoard(char board[][size]) 
 {
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++) 
 		{
-			std::cout << board[i][j];
+			std::cout << board[j][i];
 		}
 		std::cout << "\n";
 	}
